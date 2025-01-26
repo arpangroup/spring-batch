a11882adf1f68f85eedefcb3074bebe1d38e6084
arpan jana <arpangroup1@gmail.com>
2025-01-26 12:12:57 +0530

    initial commit

---
+++ cloud-native-batch/batch-job/src/main/java/com/arpan/batch/BatchJobApplication.java
@@ -0,0 +1,13 @@
+package com.arpan.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchJobApplication.class, args);
+	}
+
+}
---
+++ cloud-native-batch/batch-job/src/test/java/com/arpan/batch/BatchJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpan.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
